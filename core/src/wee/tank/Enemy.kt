8c6d143b24ab332d95301e211379f7f498f20f10
ryuryu <ryuryu.ymj@gmail.com>
2020-03-05 15:58:13 +0900

    EnemyにTankを継承

--- core/src/wee/tank/Enemy.kt
+++ core/src/wee/tank/Enemy.kt
@@ -10,46 +10,18 @@ import kotlin.math.atan2
 import kotlin.math.sqrt
 import kotlin.random.Random
 
-class Enemy : Actor() {
-    private val textureBody = Texture("tank_body.png")
-    private val textureGun = TextureRegion(Texture("tank_gun.png"))
-    /** 衝突判定用の枠 */
-    var rect = Rectangle()
-
+class Enemy : Tank() {
     private var target = Vector2(0f, 0f)
-    /** 砲台の角度<p>
-     * 三時の方向から反時計回りにラジアン */
-    var angle = 0f
     /** 弾を発射したいか否か */
     var wantToShot = false
     private var cnt = 0
-
-    init {
-        /* 拡大・縮小時も滑らかにする. */
-        textureBody.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
-
-        width = textureBody.width.toFloat()
-        height = textureBody.height.toFloat()
-
-        setPosition(STAGE_WIDTH / 2, STAGE_HEIGHT / 2)
-    }
 
     override fun act(delta: Float) {
         if (wantToShot) wantToShot = false
         if (cnt % 60 == 0) wantToShot = true
 
-        rect.set(x - width / 2 + 10, y - height / 2 + 10, width - 20, height - 20)
         cnt++
-    }
-
-    override fun draw(batch: Batch, parentAlpha: Float) {
-        batch.run {
-            setColor(color.r, color.g, color.b, color.a * parentAlpha)
-            draw(textureBody, x - width / 2, y - height / 2)
-            //draw(textureGun, x - width / 2, y - height / 2)
-            draw(textureGun, x - width / 2, y - height / 2, width / 2, height / 2,
-                    width, height, 1f, 1f, Math.toDegrees(angle.toDouble()).toFloat())
-        }
+        super.act(delta)
     }
 
     /**
